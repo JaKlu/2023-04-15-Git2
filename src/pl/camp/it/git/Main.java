7971ed667f9b552a1ffb8013efc6fbc95c419b56
JaKlu <kluska.jakub@gmail.com>
2023-04-15 09:48:31 +0200

    up2

--- src/pl/camp/it/git/Main.java
+++ src/pl/camp/it/git/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("GIT2");
         System.out.println("Cos nowego");
+        System.out.println("Nowsze");
     }
 
 }
